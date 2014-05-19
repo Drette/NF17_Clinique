01061dccdb26556d211cd6dcc343fdbf2a4cfe8f
Drette <mouffinette@hotmail.fr>
2014-05-19 19:20:55 +0200

    Create ChoixEspece.php
    Permet de choisir l'espèce de l'animal à ajouter en base ou de créer une nouvelle espèce si elle n'existe pas encore

---
+++ ChoixEspece.php
@@ -0,0 +1,29 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html charset=UTF-8">
+<h1> Formulaire d'inscription d'un animal à la clinique </h1>
+<h2> L'espèce de votre animal </h2>
+<body>
+<form method="POST" action="FormAnimal.php">
+
+<p>Choisir son espèce : </p>
+
+
+<?php
+include "connect.php";
+$vConn=mConnect();
+echo "<select name='espece'>";  
+$vSql = "SELECT nom_e FROM projet.espece;"; 
+$vQuery=pg_query($vConn, $vSql);
+while ($vResult = pg_fetch_array($vQuery, null,PGSQL_ASSOC))
+{ 
+	echo "<option value=".$vResult['nom_e'].">".$vResult['nom_e']."</option>"; 
+} 
+echo "</select>";  
+?>  
+
+<p><a href="RajoutEspece.html">Votre espèce n'existe pas ? Ajouter en une  !</a> 
+</p>
+
+<input type="submit" value="Continuez" />
+</form>
+</html>
