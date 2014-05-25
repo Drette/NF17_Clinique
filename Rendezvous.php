038a26163dd2c7291b6257d02295b00b585bac81
Drette <mouffinette@hotmail.fr>
2014-05-25 16:58:48 +0200

    Update Rendezvous.php

--- Rendezvous.php
+++ Rendezvous.php
@@ -1,10 +1,46 @@
+<!doctype html>
 <html>
-<meta http-equiv="Content-Type" content="text/html charset=UTF-8">
+<!--<meta http-equiv="Content-Type" content="text/html charset=UTF-8"> -->
+<meta charset="utf-8">
 <h1> Formulaire de prise de rendez vous à la clinique </h1>
 <h2> Choississez votre date </h2>
 <body>
-<form method="POST" action="AjoutRendezvous.php">
-<p>Entrez la date et l'heure   : <input type="date" name="date" max="2012-06-25" min="2011-08-13"/></p>
+<form method="POST" action="PriseRendezvous.php">
+<!--<p>Entrez la date et l'heure   : <input type="date" name="date" max="2012-06-25" min="2011-08-13"/></p> -->
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+<script src="jquery.ui.datepicker-fr.js"></script>
+<link rel="stylesheet" href="/resources/demos/style.css">
+<script>
+$(function() {
+//$.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );
+$( "#datepicker" ).datepicker({ minDate: 0, maxDate: "+6M",dateFormat: 'yy mm dd',firstDay:1, onSelect: function(e, b){a = new Date(); a.setFullYear(b.selectedYear, b.selectedMonth, b.selectedDay); if(a.getDay() == 6 || a.getDay() == 0) $(b.input).val(b.lastVal);}});
+// les jours 6 et 0 sont les samedis et dimanches qui ne sont plus selectionnables
+
+//$( "#datepicker" ).datepicker( $.datepicker.regional[ "fr" ] );
+
+
+
+
+});
+</script>
+<p>Date (les vétérinaires ne prennent pas de rendez vous le samedi et dimanche) : <input type="text" id="datepicker" name="date"/></p>
+
+<p>Choisir l'heure : 
+<select name="heure" >
+    <option value="8">8 heure</option>
+    <option value="9">9 heure</option>
+    <option value="10">10 heure</option>
+    <option value="11">11 heure</option>
+    <option value="12">12 heure</option>
+    <option value="14">13 heure</option>
+    <option value="15">14 heure</option>
+    <option value="16">15 heure</option>
+    <option value="17">16 heure</option>
+</select>
+<!-- On considère que tous les rendez vous durent une heure -->
 
 
 <p>Choissisez l'animal pour le rendez vous   : </p>
@@ -34,9 +70,13 @@ $vConn=mConnect();
     echo"<td><INPUT type= 'radio' name='code' value='$vResult[code_animal]'></td>";
     echo "</tr>";
   }
-
-// Date et heure libre dans la table Rendez vous vérifier que le véto n'en n'a pas d'autre à cette heure là sinon incrémentation du num du véto?
 ?>
+
+
+
+
+<!-- Date et heure libre dans la table Rendez vous vérifier que le véto n'en n'a pas d'autre à cette heure là sinon incrémentation du num du véto? -->
+
 </table>
 
 <input type="submit" value="Enregistrer" />
