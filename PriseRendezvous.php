3658ecb36c6928e99bb8d9c0791462d0a37bb697
Drette <mouffinette@hotmail.fr>
2014-05-25 16:56:59 +0200

    Update PriseRendezvous.php

--- PriseRendezvous.php
+++ PriseRendezvous.php
@@ -6,6 +6,7 @@
 <?php 
 include "connect.php";
 $vConn=mConnect();
+session_start();
 
 if (empty($_POST['date']))
 	{
@@ -14,21 +15,61 @@ if (empty($_POST['date']))
 	}
 else	
 	{
-	$vNumAnimal=$_SESSION['code']; // on récupère l'id de l'animal via la session avec le tableau
+	$vNumAnimal=$_POST['code'];
 	$vDate=$_POST['date'];
+	$vHeure=$_POST['heure'];
+	$vAnnee=substr($vDate, 0, 4);
+	$vMois=substr($vDate, -5, 2);
+	$vJour=substr($vDate, 8, 2);
+	echo "Annee =$vAnnee\n";
+	echo "Mois =$vMois\n";
+	echo "Jour =$vJour\n";
+
+	$vdate1=mktime($vHeure,0,0,$vMois,$vJour,$vAnnee);
+	$vdate2=date("Y-m-d H:i:s",$vdate1);	
+
 // le veto
 
-	$vSql = "SELECT v.num_veterinaire FROM  projet.veterinaire as v LEFT OUTER JOIN projet.rendezvous as rdv  ON v.num_veterinaire=rdv.veterinaire WHERE rdv.date <>'$vDate';"; 
-// vérifier aussi que les rendez vous ne se chevauchent pas
+	$vSql = "SELECT v.num_veterinaire FROM projet.veterinaire as v LEFT OUTER JOIN projet.rendezvous as rdv  ON v.num_veterinaire=rdv.veterinaire WHERE rdv.date ='".$vdate2."';"; 
+// On récupère les vétos déjà occupés à l'heure et la date demandées par le client
 	$vQuery=pg_query($vConn, $vSql);
-	if ($vResult = pg_fetch_array($vQuery, null,PGSQL_ASSOC)) // if car on récupère au hasard un vétérinaire libre à ce moment là
-		$vVeto= $vResult['num_veterinaire'];
-	
-	echo "Les informations suivantes ont été enregistrées :num = '$vNumAnimal' date = '$vDate', véto ='$vVeto'";
+	while ($vResult = pg_fetch_array($vQuery, null,PGSQL_ASSOC)) // on place les vétos déjà occupés dans un tableau
+		$vArray[]= $vResult['num_veterinaire'];
+	print_r($vArray);
 
-	$vSql = "INSERT INTO projet.rendezvous(animal,veterinaire,date) VALUES( '$vNumAnimal','$vDate','$vVeto');"; 
+
+$vSql = "SELECT v.num_veterinaire FROM projet.veterinaire as v;"; 
+// On récupère tous les vétérinaires
+	$vVetofinal=-1;
 	$vQuery=pg_query($vConn, $vSql);
+	while ($vResult = pg_fetch_array($vQuery, null,PGSQL_ASSOC))
+		{
+			$vOccupe=-1;
+			$vVeto= $vResult['num_veterinaire']; // on teste pour chaque vétérinaire si il est occupé à ce moment là ou pas
+			echo "Num véto=$vVeto<br>";			
+			foreach ($vArray as $i => $value) 
+			{
+				echo " valeur dans tab $value<br>";
+				if(!strcmp($vVeto,$value))
+					$vOccupe=0;
+				echo " valeur de occupe $vOccupe<br>";
+			}
+			if($vOccupe==-1) // si il n'est pas occupé on récupère sa valeur dans le tableau
+				$vVetofinal=$vVeto;
+		}
+	if($vVetofinal==-1)
+	{
+		echo"Votre rendez vous est impossible, aucun vétérinaire n'est disponible<br>";
+	
 	}
+	else
+	{
+		echo "Les informations suivantes ont été enregistrées :num = '$vNumAnimal' date = '$vdate2', véto ='$vVetofinal'";
+
+		$vSql = "INSERT INTO projet.rendezvous(animal,veterinaire,date) VALUES( '$vNumAnimal','$vVetofinal','".$vdate2."');"; 
+		$vQuery=pg_query($vConn, $vSql);
+	}
+}
 ?>
 <p></p>
 <a href='Rendezvous.php'>Retourner au formulaire rendez vous</a>
